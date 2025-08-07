2f99567208af61e213c708d415202781c15c7a86
Louis Knight-Webb <louis@bloop.ai>
2025-08-07 20:04:51 +0100

    Fix API response

--- crates/utils/src/response.rs
+++ crates/utils/src/response.rs
@@ -1,46 +1,41 @@
-mod response {
-    use serde::Serialize;
-    use ts_rs::TS;
+use serde::Serialize;
+use ts_rs::TS;
 
-    #[derive(Debug, Serialize, TS)]
-    pub struct ApiResponse<T, E = T> {
-        success: bool,
-        data: Option<T>,
-        error_data: Option<E>,
-        message: Option<String>,
+#[derive(Debug, Serialize, TS)]
+pub struct ApiResponse<T, E = T> {
+    success: bool,
+    data: Option<T>,
+    error_data: Option<E>,
+    message: Option<String>,
+}
+
+impl<T, E> ApiResponse<T, E> {
+    /// Creates a successful response, with `data` and no message.
+    pub fn success(data: T) -> Self {
+        ApiResponse {
+            success: true,
+            data: Some(data),
+            message: None,
+            error_data: None,
+        }
     }
 
-    impl<T, E> ApiResponse<T, E> {
-        /// Creates a successful response, with `data` and no message.
-        pub fn success(data: T) -> Self {
-            ApiResponse {
-                success: true,
-                data: Some(data),
-                message: None,
-                error_data: None,
-            }
+    /// Creates an error response, with `message` and no data.
+    pub fn error(message: &str) -> Self {
+        ApiResponse {
+            success: false,
+            data: None,
+            message: Some(message.to_string()),
+            error_data: None,
         }
-
-        /// Creates an error response, with `message` and no data.
-        pub fn error(message: &str) -> Self {
-            ApiResponse {
-                success: false,
-                data: None,
-                message: Some(message.to_string()),
-                error_data: None,
-            }
-        }
-        /// Creates an error response, with no `data`, no `message`, but with arbitrary `error_data`.
-        pub fn error_with_data(data: E) -> Self {
-            ApiResponse {
-                success: false,
-                data: None,
-                error_data: Some(data),
-                message: None,
-            }
+    }
+    /// Creates an error response, with no `data`, no `message`, but with arbitrary `error_data`.
+    pub fn error_with_data(data: E) -> Self {
+        ApiResponse {
+            success: false,
+            data: None,
+            error_data: Some(data),
+            message: None,
         }
     }
 }
-
-// Re-export the type, but its fields remain private, a hack to prevent direct use of the struct
-pub use response::ApiResponse;
