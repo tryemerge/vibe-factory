c44edf33cbe2368870de7f9bc005b972804137a4
Britannio Jarrett <33752528+britannio@users.noreply.github.com>
2025-09-18 08:15:54 +0000

    open the frontend by default when running the dev command (#717)
    * open the frontend by default when running the dev command
    
    * change the VITE_OPEN default to false

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -73,6 +73,7 @@ export default defineConfig({
     fs: {
       allow: [path.resolve(__dirname, "."), path.resolve(__dirname, "..")],
     },
+    open: process.env.VITE_OPEN === "true",
   },
   build: { sourcemap: true },
 });
