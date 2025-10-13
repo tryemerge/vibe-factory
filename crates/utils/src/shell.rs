735bbcfeff0ab7fb4ba568ab205ce8f189293c67
Solomon <abcpro11051@disroot.org>
2025-10-13 09:51:34 +0100

    Try zsh, bash then sh (#1000)

--- crates/utils/src/shell.rs
+++ crates/utils/src/shell.rs
@@ -9,8 +9,10 @@ pub fn get_shell_command() -> (&'static str, &'static str) {
     if cfg!(windows) {
         ("cmd", "/C")
     } else {
-        // Prefer bash if available, fallback to sh
-        if std::path::Path::new("/bin/bash").exists() {
+        // Prefer zsh or bash if available, fallback to sh
+        if std::path::Path::new("/bin/zsh").exists() {
+            ("zsh", "-c")
+        } else if std::path::Path::new("/bin/bash").exists() {
             ("bash", "-c")
         } else {
             ("sh", "-c")
